5a0f68be1ac9696bfe5f17ff92721f33759bee88
manohar <manohar@gmail.com>
2026-06-11 04:58:23 +0000

    Added: Terraform variables

---
+++ 03-variables/main.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "test_ec2" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.test_sg.id]
+    tags = var.ec2_tags
+}
+
+resource "aws_security_group" "test_sg" {
+  name = "test-sg"
+  description = "Allow SSH"
+  
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.sg_tags
+}
