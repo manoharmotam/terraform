323677a2047240eb2efa87ee8a312dda4add82a9
manoharmotam <b3stsinceday1@gmail.com>
2026-06-17 16:58:51 +0000

    Added: workspaces Example

--- 15-workspaces/local.tf
+++ 15-workspaces/local.tf
@@ -1,15 +1,15 @@
 locals {
-  environment = terraform.workspace
-  ami_id = "ami-02dfbd4ff395f2a1b"
+  environment   = terraform.workspace
+  ami_id        = "ami-02dfbd4ff395f2a1b"
   instance_type = "t3.micro"
   tags = merge(var.common_tags,
-        {
-            "Managed By" = "terraform"
-        }
-    )
+    {
+      "Managed By" = "terraform"
+    }
+  )
   project = "roboshop"
   ssh = [{
-    port = 22
+    port        = 22
     cidr_blocks = ["${chomp(data.http.get_ip.response_body)}/32"]
   }]
 }
--- 15-workspaces/variables.tf
+++ 15-workspaces/variables.tf
@@ -3,13 +3,13 @@ variable "instance_type" {
 
   validation {
     error_message = "Instance size should be either 't3.micro' or 'c7i-flex.large'"
-    condition = contains(["t3.micro", "c7i-flex.large"], var.instance_type)
+    condition     = contains(["t3.micro", "c7i-flex.large"], var.instance_type)
   }
 }
 
 variable "common_tags" {
   default = {
-    Project = "roboshop"
+    Project     = "roboshop"
     Environment = "dev"
   }
 }
@@ -18,7 +18,7 @@ variable "allowed_ports" {
   type = list(any)
   default = [
     {
-      port = 80
+      port        = 80
       cidr_blocks = ["0.0.0.0/0"]
     }
   ]
